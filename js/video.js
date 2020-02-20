3641d3e1ed74dec79d7e4cb3907e113211084cb7
cooper62498 <cselig@umich.edu>
2020-02-20 10:49:36 -0500

    almost done

--- js/video.js
+++ js/video.js
@@ -7,46 +7,67 @@ function getVid(){
 function playVid() { 
 
 	console.log("Play Video");
+	if (video.paused)
+		video.play();
+		changeVolume();
 } 
 
 function pauseVid() { 
 
 	console.log("Pause Video");
+	if (!video.paused)
+		video.pause();
 } 
 
 function decreaseSpeed() { 
-
-  	console.log("Speed is "+ );
+	video.playbackRate = video.playbackRate * .8;
+	console.log("Speed is "+ video.playbackRate);
+	 
 } 
 
 function increaseSpeed() {
-
-	console.log("Speed is "+ );
+	video.playbackRate = video.playbackRate * 1.25;
+	console.log("Speed is "+ video.playbackRate);
 } 
 
 function skipAhead() {
-
-	console.log("Current location is "+ );
+	video.currentTime = video.currentTime + 60;
+	if (video.ended) {
+		video.currentTime = 0;
+		pauseVid();
+		video.playbackRate = 1;
+	}
+	console.log("Current location is "+ video.currentTime);
 } 
 
 function mute() { 
-  	
-  		console.log("Unmuted");
-  	
-      	console.log("Muted");
+  	if (video.muted) {
+		video.muted = false;
+		document.querySelector("#mute").textContent = "Mute";
+		console.log("Unmuted");
+	  }
+	else {
+		video.muted = true;
+		document.querySelector("#mute").textContent = "Unmute";
+		console.log("Muted");
+	}  	
 }
 
 function changeVolume() {
-;	console.log("Volume is ");
+	var volume = document.querySelector("#volumeSlider").value;
+	video.volume = volume / 100;
+	var volume_span = document.querySelector("#volume");
+	volume_span.textContent = volume + "%";
+	console.log("Volume is " + volume);
 }
        
 
 function gray() { 
-
+	video.classList.add("grayscale")
 	console.log("In grayscale")
 }
 
 function color() {
-
+	video.classList.remove("grayscale")
 	console.log("In color") 
 }
